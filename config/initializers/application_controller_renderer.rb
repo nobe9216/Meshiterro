2df6702c088309a6bd3f5a416c464275d399b0c2
nobe9216 <nobe9216@gmail.com>
2021-05-20 15:59:30 +0000

    [Finish]study_5-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'df4198951febc61395649b6c6a58fff5e66809220171d26e3357212cdb949c684b1889a1e0c0b7e4b14e7439903fd3df8b6fc3bcef872648f4d7cc593fe115c1'
